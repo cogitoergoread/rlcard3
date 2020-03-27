82d556a306fd240c7c081ee69ed47a831f1822ff
Varga GHupi Jozsef <31209755+cogitoergoread@users.noreply.github.com>
2020-03-27 21:09:14 +0100

    examples/mocsar_multi_agent human, random, min agentekre teszt.

---
+++ examples/mocsar_multi_agent.py
@@ -0,0 +1,25 @@
+import rlcard
+from rlcard.games.mocsar.stat import MocsarStat
+
+# Make environment and enable human mode
+env = rlcard.make('mocsar-cfg', config={'multi_agent_mode': True})
+
+# Register agents
+# agents = {"mocsar_random": 2, "mocsar_min": 2}
+agents = {"mocsar_random": 1, "mocsar_human": 1, "mocsar_min": 2}
+env.model.create_agents(agents)
+
+print(f"Players: {env.game.players.__repr__()}")
+stat = MocsarStat(game=env.game, agents=env.model.rule_agents, nr_of_games=1)
+
+# Run the game
+state, payoffs, done = env.run_multi_agent(stat=stat)
+
+stat.add_result(payoffs=payoffs)
+
+stat.write_to_file()
+
+print(f"Players: {env.game.players.__repr__()}")
+print(f"Agents:{[ ag.__str__() for ag in env.model.rule_agents]}")
+print(f"Payoffs:{payoffs}")
+print(f"State:{state}")
