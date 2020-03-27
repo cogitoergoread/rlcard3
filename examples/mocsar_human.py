047145cb748b36935a0ac60110ba65df3b872f2e
Varga GHupi Jozsef <31209755+cogitoergoread@users.noreply.github.com>
2020-03-27 05:55:49 +0100

    examples/mocsar_human Ok, lefut

--- examples/mocsar_human.py
+++ examples/mocsar_human.py
@@ -4,18 +4,20 @@
 import rlcard
 
 # Make environment and enable human mode
-env = rlcard.make(env_id= 'mocsar', config={'human_mode' : True})
+env = rlcard.make(env_id='mocsar', config={'human_mode': True})
 
 # Reset environment
 state = env.reset()  # TODO ez még nem fut
 
 while not env.is_over():
+    legal_actions = state['legal_actions']
+    legal_actions.insert(0, 0)
     action = input('>> You choose action (integer): ')
     if action == '-1':
         print('Break the game...')
         break
     while not action.isdigit() \
-            or int(action) not in state['legal_actions']:
+            or int(action) not in legal_actions:
         print('Action illegal...')
         action = input('>> Re-choose action (integer): ')
     state, reward, done = env.step(int(action))
