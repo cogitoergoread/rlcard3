122a238c820a21d47d5e0327a25b600beeaee971
Varga GHupi Jozsef <31209755+cogitoergoread@users.noreply.github.com>
2020-03-31 17:45:28 +0200

    mocsar DQN

---
+++ examples/mocsar_dqn_pytorch_load_model_2.py
@@ -0,0 +1,24 @@
+''' Another example of loading a pre-trained NFSP model on Leduc Hold'em
+    Here, we directly load the model from model zoo
+'''
+import rlcard3
+from rlcard3.agents.random_agent import RandomAgent
+from rlcard3.utils.utils import set_global_seed, tournament
+from rlcard3 import models
+
+# Make environment
+env = rlcard3.make('mocsar')
+
+# Set a global seed
+set_global_seed(0)
+
+# Here we directly load NFSP models from /models module
+dqn_agents = models.load('mocsar-dqn-pytorch').agents
+
+# Evaluate the performance. Play with random agents.
+evaluate_num = 500
+random_agent = RandomAgent(env.action_num)
+env.set_agents([dqn_agents[0], random_agent, random_agent, random_agent])
+reward = tournament(env, evaluate_num)[0]
+print('Average reward against random agent: ', reward)
+
