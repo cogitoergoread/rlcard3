3e8b8bedb7fd267140a3987e93c5ccede6a2f9ff
Varga GHupi Jozsef <31209755+cogitoergoread@users.noreply.github.com>
2020-04-01 20:18:08 +0200

    state encode obs bizonyos agentek számára, debug ki

--- rlcard3/envs/mocsar_cfg.py
+++ rlcard3/envs/mocsar_cfg.py
@@ -51,15 +51,12 @@ class MocsarCfgEnv(MocsarEnv):
         while not self.is_over():
             agent = self.model.agents[player_id]
             a_extract = get_by_id(aid=agent.id).a_extracted
-            print(f"Multi, ag:{a_extract}, s:{state['is_extract']}")
             if a_extract and not state['is_extract']:
-                print("State Conv")
                 # Az Agent számára obs status kell, de nem az van
                 step_state = {'obs': encode_to_obs(state=state),
                            'legal_actions': [string_to_action(action) for action in state['legal_actions'] ] ,
                            'is_extract': True  # State is extracted>
                            }
-                print(step_state)
             else:
                 step_state =state
             if self.model.use_raw:
